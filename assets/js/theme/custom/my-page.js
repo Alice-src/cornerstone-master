9c12ca365ceea21b4a15f8c5643aab99e0908ded
Alice-src <milky.sweeet@gmail.com>
2021-07-10 21:32:11 +0300

    new

---
+++ assets/js/theme/custom/my-page.js
@@ -0,0 +1,12 @@
+import PageManager from '../page-manager';
+
+/*export default class Faq extends PageManager {
+    onReady() {
+        this.logNumber();
+    }
+    logNumber() {
+        function aaaFunction() {
+
+        };
+}
+}*/
